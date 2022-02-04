eec3f7219a12d640a6e61f7af300d624e67e1d95
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-02-04 15:02:16 +0300

    7. hafta 1. ödev yapıldı.

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ConvertComponent } from './CurrencyExchange/convert/convert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConvertComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
